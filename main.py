b8bf85b83bd42f30d66574732306c7f0bffe73a5
Karol Dzialowski <karlososhd@gmail.com>
2016-02-01 12:42:05 +0100

    :thumbsup: Aktualizacja komendy /help

--- main.py
+++ main.py
@@ -57,9 +57,30 @@ Mozesz mnie kontrolowac uzywajac tych komend:
 /d - Plan na dzisiaj
 
 /remind data "wiadomosc" - przypomninanie
-data w formacie: 1d, 1m, tomorrow, eom, 2016-03-02, itd
+data w formacie:
+
+    August 25th, 2008
+    25 Aug 2008
+    Aug 25 5pm
+    5pm August 25
+    next saturday
+    tomorrow
+    next thursday at 4pm
+    at 4pm
+    eod
+    tomorrow eod
+    eod tuesday
+    eoy
+    eom
+    in 5 minutes
+    5 minutes from now
+    5 hours before now
+    2 hours before noon
+    2 days from tomorrow
 
 /stats - wyswietla statystyki czatu
+
+/weekstats - wyświetla statystyki czatu liczone od poprzedniej niedzieli
 
 Mozesz tez uzywac wydluzonych komend, np, /dzisiaj, /poniedzialek, /nastepna, /jutro itd."""
 
